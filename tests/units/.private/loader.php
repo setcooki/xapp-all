bced5cd2804c0ba3d3761682253dfa0e1961babf
mc007 <asd28071977>
2015-01-17 11:24:34 +0100

    =

--- tests/units/.private/loader.php
+++ tests/units/.private/loader.php
@@ -1,12 +1,12 @@
 <?php
 
-$XAPP_BASE_DIRECTORY =  realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '../../xapp/');
+$XAPP_BASE_DIRECTORY =  realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '../../../xapp/');
 
 //pull in function.php
-require_once(realpath(dirname(__FILE__) . '/../../autoload.php'));
+require_once(realpath(dirname(__FILE__) . '/../../../autoload.php'));
 
 //pull in function.php
-require_once(realpath(dirname(__FILE__) . '/../functions.php'));
+require_once(realpath(dirname(__FILE__) . '/../../functions.php'));
 
 //pull in Bootstrap.php
 require_once(realpath(dirname(__FILE__) . '/Bootstrap.php'));
--- tests/units/config/test.php
+++ tests/units/config/test.php
@@ -4,7 +4,7 @@
 //
 //  Basic deps and setup
 //
-require_once dirname(__FILE__) . '/../loader.php';
+require_once dirname(__FILE__) . '/../.private/loader.php';
 
 /**
  * configure and run dependencies
@@ -52,105 +52,42 @@ class ConfigTest extends PHPUnit_Framework_TestCase{
 		$this->assertEquals($result, 'test', 'XApp_Config::retrieve failed: expected: ' . 'test');
 
 	}
+
+	public function testIni(){
+
+		$config = '
+		value1 = test
+        ';
+
+		$config = Xapp_Config::create($config, 'priv2', 'ini');
+		$result = Xapp_Config::retrieve('priv1', 'value1');
+
+		$this->assertEquals($result, 'test', 'XApp_Config::retrieve failed: expected: ' . 'test');
+
+	}
+
+	public function testJson(){
+
+		$config =
+		'
+	    {"value1": "test", "value2": {"value3": "test"}}
+        ';
+
+		$config = Xapp_Config::create($config, 'priv3', 'json');
+		$result = Xapp_Config::retrieve('priv3', 'value2.value3');
+		$this->assertEquals($result, 'test', 'XApp_Config::retrieve failed: expected: ' . 'test');
+
+	}
+	public function testXml(){
+
+		$config =
+		'
+	    <config><value1>test</value1><value2><value3>test</value3></value2></config>
+	    ';
+		$config = Xapp_Config::create($config, 'priv4', 'xml');
+		$result = Xapp_Config::retrieve('priv4', 'value2.value3');
+
+		$this->assertEquals($result, 'test', 'XApp_Config::retrieve failed: expected: ' . 'test');
+
+	}
 }
-
-
-
-//_print("testing xapp/Config module:");
-
-
-/*
-
-
-if(is_dir(dirname(__FILE__) . '/vendor'))
-{
-    if(isset($_GET['custom']) || (php_sapi_name() === 'cli' && isset($argv[1]) && $argv[1] === 'custom'))
-    {
-        require_once dirname(__FILE__) . '/vendor/xapp/Core/core.php';
-    }else{
-        require_once dirname(__FILE__) . '/vendor/autoload.php';
-        require_once dirname(__FILE__) . '/vendor/xapp/Core/core.php';
-    }
-}else{
-    die("... module missing - please install with composer");
-}
-
-$conf = array
-(
-    XAPP_CONF_DEBUG_MODE => false,
-    XAPP_CONF_AUTOLOAD => false,
-    XAPP_CONF_DEV_MODE => true,
-    XAPP_CONF_HANDLE_BUFFER => false,
-    XAPP_CONF_HANDLE_SHUTDOWN => false,
-    XAPP_CONF_HTTP_GZIP => false,
-    XAPP_CONF_CONSOLE => false,
-    XAPP_CONF_HANDLE_ERROR => false,
-    XAPP_CONF_HANDLE_EXCEPTION => false,
-    XAPP_CONF_LOG_ERROR => false,
-);
-
-xapp_conf($conf);
-
-xapp_import('xapp.Config.*');
-
-try
-{
-
-    $config = array
-    (
-        'value1' => 'test',
-        'value2' => array('value3' => 'test')
-    );
-
-    $conf = Xapp_Config::create($config, 'priv1', 'php');
-    if(Xapp_Config::retrieve('priv1', 'value2.value3') === 'test')
-    {
-        _print("... xapp/Config/Php - OK");
-    }else{
-        _print("... xapp/Config/Php - NOT OK");
-    }
-
-
-    $config =
-    '
-    value1 = test
-    ';
-    $config = Xapp_Config::create($config, 'priv2', 'ini');
-    if(Xapp_Config::retrieve('priv2', 'value1') === 'test')
-    {
-        _print("... xapp/Config/Ini - OK");
-    }else{
-        _print("... xapp/Config/Ini - NOT OK");
-    }
-
-
-    $config =
-    '
-    {"value1": "test", "value2": {"value3": "test"}}
-    ';
-    $config = Xapp_Config::create($config, 'priv3', 'json');
-    if(Xapp_Config::retrieve('priv3', 'value2.value3') === 'test')
-    {
-        _print("... xapp/Config/Json - OK");
-    }else{
-        _print("... xapp/Config/Json - NOT OK");
-    }
-
-
-    $config =
-    '
-    <config><value1>test</value1><value2><value3>test</value3></value2></config>
-    ';
-    $config = Xapp_Config::create($config, 'priv4', 'xml');
-    if(Xapp_Config::retrieve('priv4', 'value2.value3') === 'test')
-    {
-        _print("... xapp/Config/Xml - OK");
-    }else{
-        _print("... xapp/Config/Xml - NOT OK");
-    }
-}
-catch(Exception $e)
-{
-    _print("... xapp/Config - " . $e->getMessage());
-}
-*/
